001c9563a6bb758d222346fc4221d515ddcc8f37
valchara <charyevalery@mail.ru>
2025-11-28 21:47:53 +0300

    Убрала лишний импорт из errorhandler

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
