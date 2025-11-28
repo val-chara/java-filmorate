c0fd44345e915ea39748be296230c6bca0ad83f3
valchara <charyevalery@mail.ru>
2025-11-28 21:12:02 +0300

    Добавила модели Film и User

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private int id;
+
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Электронная почта должна содержать символ @")
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
+    private String login;
+
+    private String name;
+
+    @Past(message = "Дата рождения не может быть в будущем времени")
+    private LocalDate birthday;
+}
