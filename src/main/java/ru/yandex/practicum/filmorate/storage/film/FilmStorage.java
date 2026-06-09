ff8bcd66f292117fceeceaa529e180dae0a9dce0
valchara <charyevalery@mail.ru>
2026-06-09 12:11:36 +0300

    Исправлен стиль

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,9 +6,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Film add(Film film);
+
     Film update(Film film);
+
     void delete(int id);
+
     List<Film> getAll();
+
     Optional<Film> getById(int id);
+
 }
