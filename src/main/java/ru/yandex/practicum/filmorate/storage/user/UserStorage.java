4fb56509a1b53bc92465e9ea12ef62da02deaef8
valchara <charyevalery@mail.ru>
2026-06-09 12:12:58 +0300

    Исправлен стиль

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,9 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User add(User user);
+
     User update(User user);
+
     void delete(int id);
+
     List<User> getAll();
+
     Optional<User> getById(int id);
 }
