64516557eb3b5ca68d1ec98583112fe72eef4438
valchara <charyevalery@mail.ru>
2026-06-10 13:35:53 +0300

    Добавила схемы таблиц

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+MERGE INTO mpa_ratings (id, name) VALUES (1, 'G');
+MERGE INTO mpa_ratings (id, name) VALUES (2, 'PG');
+MERGE INTO mpa_ratings (id, name) VALUES (3, 'PG-13');
+MERGE INTO mpa_ratings (id, name) VALUES (4, 'R');
+MERGE INTO mpa_ratings (id, name) VALUES (5, 'NC-17');
+
+MERGE INTO genres (id, name) VALUES (1, 'Комедия');
+MERGE INTO genres (id, name) VALUES (2, 'Драма');
+MERGE INTO genres (id, name) VALUES (3, 'Мультфильм');
+MERGE INTO genres (id, name) VALUES (4, 'Триллер');
+MERGE INTO genres (id, name) VALUES (5, 'Документальный');
+MERGE INTO genres (id, name) VALUES (6, 'Боевик');
