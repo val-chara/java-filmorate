153c1cb686de2e0e4c98daa6656798a21889bbcc
valchara <charyevalery@mail.ru>
2026-06-11 09:18:06 +0300

    Добавила валидацию MPA и жанров при создании фильма

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -9,6 +9,9 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
+import ru.yandex.practicum.filmorate.storage.mpa.MpaStorage;
+import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,15 +23,20 @@ public class FilmService {
 
     private final FilmStorage filmStorage;
     private final FilmDbStorage filmDbStorage;
+    private final MpaStorage mpaStorage;
+    private final GenreStorage genreStorage;
 
     @Autowired
-    public FilmService(@Qualifier("filmDbStorage") FilmStorage filmStorage) {
+    public FilmService(@Qualifier("filmDbStorage") FilmStorage filmStorage,MpaStorage mpaStorage, GenreStorage genreStorage) {
         this.filmStorage = filmStorage;
         this.filmDbStorage = (FilmDbStorage) filmStorage;
+        this.mpaStorage = mpaStorage;
+        this.genreStorage = genreStorage;
     }
 
     public Film create(Film film) {
         validateFilm(film);
+        validateMpaAndGenres(film);
         Film created = filmStorage.add(film);
         log.info("Добавлен новый фильм: {}", created);
         return created;
@@ -37,6 +45,7 @@ public class FilmService {
     public Film update(Film film) {
         getByIdOrThrow(film.getId());
         validateFilm(film);
+        validateMpaAndGenres(film);
         Film updated = filmStorage.update(film);
         log.info("Обновлён фильм: {}", updated);
         return updated;
@@ -75,6 +84,18 @@ public class FilmService {
         if (film.getReleaseDate().isBefore(MIN_RELEASE_DATE)) {
             log.error("Дата релиза {} раньше минимальной допустимой {}", film.getReleaseDate(), MIN_RELEASE_DATE);
             throw new ValidationException("Дата релиза не может быть раньше " + MIN_RELEASE_DATE);
+        }
+    }
+    private void validateMpaAndGenres(Film film) {
+        if (film.getMpa() != null) {
+            mpaStorage.getById(film.getMpa().getId())
+                    .orElseThrow(() -> new NotFoundException("Рейтинг с id " + film.getMpa().getId() + " не найден"));
+        }
+        if (film.getGenres() != null) {
+            for (Genre genre : film.getGenres()) {
+                genreStorage.getById(genre.getId())
+                        .orElseThrow(() -> new NotFoundException("Жанр с id " + genre.getId() + " не найден"));
+            }
         }
     }
 }
